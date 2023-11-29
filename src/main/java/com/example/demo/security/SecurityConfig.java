5a87915e1d1f2d988a71572f24553013cef45138
Matas <matasraila123@gmail.com>
2023-11-29 14:01:54 +0200

    a

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
             .requestMatchers("/", "/login").permitAll()
             .requestMatchers("/", "/register").permitAll()
            // .requestMatchers("/topic/{id}").hasAuthority("Admin")
-            .anyRequest().authenticated();
+            .anyRequest().permitAll();
         return http.build();
     }
 
